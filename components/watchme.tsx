8924408996d05ffed7a6ad70a884580cfecf9a69
nwald97 <98556128+nwald97@users.noreply.github.com>
2022-01-27 15:21:56 -0800

    Update watchme.tsx

--- components/watchme.tsx
+++ components/watchme.tsx
@@ -2,14 +2,14 @@ import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 
 export default function WatchMe() {
-  let [isOpen, setIsOpen] = useState(true)
+  let [isOpen, setIsOpen] = useState(false)
 
   function closeModal() {
     setIsOpen(false)
   }
 
   function openModal() {
-    setIsOpen(true)
+    setIsOpen(false)
   }
 
   return (
